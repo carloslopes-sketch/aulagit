1daec2fd891d3d2a2857576dc482aa65a38e0cb1
carloslopes-sketch <carlos.lopes@strategicconsult.com.br>
2025-11-03 21:45:34 -0300

    Delete UC1 directory

--- UC1/Aula2/Aula2.py
+++
@@ -1,7 +0,0 @@
-Nome='José Carlos'
-NotaUc1=int(input('Digite a primeira nota: '))
-NotaUc2=int(input('Digite a segunda nota: '))
-Media=((NotaUc1)+(NotaUc2))/2
-
-print('Nome ', Nome)
-print('Média ', Media)
--- UC1/Aula4/aula.4.08.py
+++
@@ -1,6 +0,0 @@
-numero=float(input('Insira o valor do numero escolhido'))
-
-if numero >= 0:
-    print('O numero escolhido é positivo')
-else:
-    print('O numero escolhido é negativo')

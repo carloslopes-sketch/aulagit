8d3cec9080773e5090d92a8dc05cde67b0c31b5c
JOSE CARLOS LOPES DE MENDONCA <jose.mendonca@aluno.senac.rio>
2025-11-07 21:43:21 -0300

    eee

---
+++ UC1/Aula8/aula.8.07.py
@@ -0,0 +1,8 @@
+def ano (ano):
+    if ano%4 or ano%400 =0
+        print('O ano X É bissexto')
+    elif ano%4 and ano%100 = 0
+        print('O ano X É bissexto')
+    elif ano%4 > 0
+        print('O ano X É bissexto')
+ano(2025)
